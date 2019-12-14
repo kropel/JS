7f72e070b88f7b83c1b8f2833e7e1275dbd59f92
Radek Kuczynski <krooopel@gmail.com>
2019-12-14 21:48:49 +0100

    Add autorun

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-let kill = (function(){
-    let timer;
-    function clickX(){
-        let Xbutton = document.querySelector('.ytp-ad-overlay-close-button');
-        if(!!Xbutton){
-            console.log(Xbutton);
-            Xbutton.click();
-        }
+let kill = (function() {
+  let timer;
+  function clickX() {
+    let Xbutton = document.querySelector(".ytp-ad-overlay-close-button");
+    if (!!Xbutton) {
+      console.log(Xbutton);
+      Xbutton.click();
     }
-        function video(){
-            let countdown = document.querySelector('.ytp-ad-preview-container.countdown-next-to-thumbnail');
-            let skip = document.querySelector('.ytp-ad-skip-button .ytp-button');
-            if(!!countdown || !!skip){
-                console.log(countdown+"\n"+skip);
-                let video = document.querySelector('.html5-video-container > .video-stream');
-                video.currentTime = video.getDuration();
-            }
-        }
-        function init(){
-            clickX();
-            video();
-        }
-    function start(){
-        timer = setInterval(init, 1000);
+  }
+  function video() {
+    let countdown = document.querySelector(
+      ".ytp-ad-preview-container.countdown-next-to-thumbnail"
+    );
+    let skip = document.querySelector(".ytp-ad-skip-button .ytp-button");
+    if (!!countdown || !!skip) {
+      console.log(countdown + "\n" + skip);
+      let video = document.querySelector(
+        ".html5-video-container > .video-stream"
+      );
+      video.currentTime = video.getDuration();
     }
-    function stop(){
-        clearInterval(timer);
-    }
-    return {
-        start: start,
-        stop: stop,
-    };
+  }
+  function init() {
+    clickX();
+    video();
+  }
+  function start() {
+    timer = setInterval(init, 1000);
+  }
+  function stop() {
+    clearInterval(timer);
+  }
+  start();
+  return {
+    start: start,
+    stop: stop
+  };
 })();
